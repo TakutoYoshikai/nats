d89c856c08746d85f36912f0aa76dd1250019d64
takuto <takuto.yoshikai@gmail.com>
2020-08-14 22:03:53 +0900

    tmp

---
+++ src/encrypter.rs
@@ -0,0 +1,239 @@
+
+extern crate crypto;
+extern crate rand;
+use std::io::Read;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::option::Option;
+
+use crypto::{ buffer, aes, blockmodes };
+use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
+use rand::Rng;
+use rand::OsRng;
+
+fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Option<Vec<u8>> {
+    let mut encryptor = aes::cbc_encryptor(
+            aes::KeySize::KeySize256,
+            key,
+            iv,
+            blockmodes::PkcsPadding);
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+    loop {
+        let result = encryptor.encrypt(&mut read_buffer, &mut write_buffer, true);
+        match result {
+            Ok(_) => (),
+            Err(_) => return None,
+        }
+        let result = result.unwrap();
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => { }
+        }
+    }
+    Some(final_result)
+}
+
+fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Option<Vec<u8>> {
+    let mut decryptor = aes::cbc_decryptor(
+            aes::KeySize::KeySize256,
+            key,
+            iv,
+            blockmodes::PkcsPadding);
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+    loop {
+        let result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true);
+        match result {
+            Ok(_) => (),
+            Err(_) => return None,
+        }
+        let result = result.unwrap();
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => { }
+        }
+    }
+    Some(final_result)
+}
+
+fn load_key(path: &str) -> ([u8; 32], [u8; 16]) {
+    let mut f = File::open(path).expect("failed");
+    let metadata = fs::metadata(path).expect("failed");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("failed");
+    let mut key: [u8; 32] = [0; 32];
+    let mut iv: [u8; 16] = [0; 16];
+    for (i, byte) in buffer.iter().enumerate() {
+        if i < 32 {
+            key[i] = *byte;
+        } else {
+            iv[i - 32] = *byte;
+        }
+    }
+    return (key, iv);
+}
+
+fn encrypt_file(path: &str, key: &[u8], iv: &[u8]) -> Option<Vec<u8>>{
+    let f = File::open(path);
+    match f {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let mut f = f.unwrap();
+    let metadata = fs::metadata(path);
+    match metadata {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let metadata = metadata.unwrap();
+    let mut buffer = vec![0; metadata.len() as usize];
+    match f.read(&mut buffer) {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let encrypted_data = encrypt(&buffer, &key, &iv);
+    return encrypted_data;
+}
+
+fn decrypt_file(path: &str, key: &[u8], iv: &[u8]) -> Option<Vec<u8>>{
+    let f = File::open(path);
+    match f {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let mut f = f.unwrap();
+    let metadata = fs::metadata(path);
+    match metadata {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let metadata = metadata.unwrap();
+    let mut buffer = vec![0; metadata.len() as usize];
+    match f.read(&mut buffer) {
+        Ok(_) => (),
+        Err(_) => return None,
+    }
+    let decrypted_data = decrypt(&buffer, &key, &iv);
+    if decrypted_data.is_none() {
+        println!("{}", path);
+    }
+    return decrypted_data;
+}
+
+fn save_file(path: &str, data: &Vec<u8>) {
+    let out = File::create(path);
+    match out {
+        Ok(_) => (),
+        Err(_) => return,      
+    }
+    let out = out.unwrap();
+    let mut writer = BufWriter::new(out);
+    match writer.write(data) {
+        Ok(_) => (),
+        Err(_) => (),
+    }
+}
+
+fn get_files_and_dirs(dirpath: &str) -> (Vec<String>, Vec<String>) {
+    let mut files: Vec<String> = Vec::new();
+    let mut dirs: Vec<String> = Vec::new();
+    for path in fs::read_dir(dirpath).unwrap() {
+        let _path = path.unwrap().path();
+        if _path.is_dir() {
+            dirs.push(_path.display().to_string());
+        } else {
+            files.push(_path.display().to_string());
+        }
+    }
+    return (files, dirs);
+}
+
+fn encrypt_dir(dirpath: &str, key: &[u8], iv: &[u8]) {
+    let files_and_dirs = get_files_and_dirs(dirpath);
+    let files: Vec<String> = files_and_dirs.0;
+    let dirs: Vec<String> = files_and_dirs.1;
+    for file in files {
+        let data = encrypt_file(&file, &key, &iv);   
+        if data.is_none() {
+            continue;
+        }
+        save_file(&file, &data.unwrap());
+    }
+    for dir in dirs {
+        encrypt_dir(&dir, key, iv);
+    }
+}
+
+fn decrypt_dir(dirpath: &str, key: &[u8], iv: &[u8]) {
+    let files_and_dirs = get_files_and_dirs(dirpath);
+    let files: Vec<String> = files_and_dirs.0;
+    let dirs: Vec<String> = files_and_dirs.1;
+    for file in files {
+        let data = decrypt_file(&file, &key, &iv);   
+        if data.is_none() {
+            continue;
+        }
+        save_file(&file, &data.unwrap());
+    }
+    for dir in dirs {
+        decrypt_dir(&dir, key, iv);
+    }
+}
+
+fn make_key_file(key: &[u8; 32], iv: &[u8; 16]) {
+    let mut data:Vec<u8> = Vec::new();
+    for (_, b) in key.iter().enumerate() {
+        data.push(*b);
+    }
+    for (_, b) in iv.iter().enumerate() {
+        data.push(*b);
+    }
+    save_file("./key", &data);
+}
+
+pub fn encrypt_and_save_key(path: &str) {
+    let mut key: [u8; 32] = [0; 32];
+    let mut iv: [u8; 16] = [0; 16];
+    let mut rng = OsRng::new().ok().unwrap();
+    rng.fill_bytes(&mut key);
+    rng.fill_bytes(&mut iv);
+    encrypt_dir(path, &key, &iv);
+    make_key_file(&key, &iv);
+}
+
+pub fn decrypt_and_load_key(dirpath: &str, keypath: &str) {
+    let (key, iv) = load_key(keypath);
+    decrypt_dir(dirpath, &key, &iv);
+}
+
+pub fn encrypt_one_file(path: &str) {
+    let mut key: [u8; 32] = [0; 32];
+    let mut iv: [u8; 16] = [0; 16];
+    let mut rng = OsRng::new().ok().unwrap();
+    rng.fill_bytes(&mut key);
+    rng.fill_bytes(&mut iv);
+    let data = encrypt_file(path, &key, &iv);
+    if data.is_none() {
+        return;
+    }
+    save_file(path, &data.unwrap());
+    make_key_file(&key, &iv);
+}
+
+pub fn decrypt_one_file(path: &str, keypath: &str) {
+    let (key, iv) = load_key(keypath);
+    let data = decrypt_file(path, &key, &iv);
+    if data.is_none() {
+        return;
+    }
+    save_file(path, &data.unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::process::{Command, Stdio};
 use std::env;
 use std::fs::File;
-use std::io::{self, Read, Write, BufReader};
+use std::io::{Write};
 extern crate rand;
+mod encrypter;
 fn exec(command: &str) {
     let mut process = Command::new("bash").arg("-c").arg(command).stdout(Stdio::piped()).spawn().expect("failed to execute");
     let _ = process.wait();
@@ -14,7 +15,7 @@ fn random_bytes(n: i64) -> Vec<u8>{
     }).collect();
 }
 
-fn write(filename: &str, data: Vec<u8>) {
+fn save(filename: &str, data: Vec<u8>) {
     let mut file = File::create(filename).unwrap();
     file.write_all(&data).expect("failed to write");
     file.flush().expect("failed to flush");
@@ -35,14 +36,23 @@ fn pack(command: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     result.extend(&last_offset());
     return result;
 }
+
+
+fn encrypt(filename: &str) {
+    encrypter::encrypt_and_save_key(filename);
+}
+
 fn nats_in() {
     let args: Vec<String> = env::args().collect();
+    /*
     exec("head -c 173 /dev/urandom > r.txt");
     exec("head -c 135 /dev/urandom > r2.txt");
     exec(&format!("natsme {}", args[3]));
     exec(&format!("cat {} r.txt {} r2.txt > {}.dm", args[2], args[3], args[2]));
     exec("rm r.txt r2.txt");
     exec(&format!("chmod +x {}.dm", args[2]));
+    */
+    encrypt(&args[3]);
 }
 
 fn nats_out() {
