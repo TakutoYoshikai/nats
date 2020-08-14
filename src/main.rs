5257d63393959c1ed5c59b3aa5e8ffc64f339c36
takuto <takuto.yoshikai@gmail.com>
2020-08-14 22:51:29 +0900

    tmp

--- src/main.rs
+++ src/main.rs
@@ -53,21 +53,24 @@ fn encrypt(filename: &str) {
 
 fn nats_in() {
     let args: Vec<String> = env::args().collect();
-    /*
-    exec("head -c 173 /dev/urandom > r.txt");
-    exec("head -c 135 /dev/urandom > r2.txt");
-    exec(&format!("natsme {}", args[3]));
-    exec(&format!("cat {} r.txt {} r2.txt > {}.dm", args[2], args[3], args[2]));
-    exec("rm r.txt r2.txt");
-    exec(&format!("chmod +x {}.dm", args[2]));
-    */
-    encrypt(&args[3]);
+    encrypter::encrypt_one_file(&args[3]);
+    let command = load(&args[2]);
+    let data = load(&args[3]);
+    let packed = pack(command, data);
+    save(&format!("{}.dm", args[2]), packed);
 }
 
 fn nats_out() {
     let args: Vec<String> = env::args().collect();
-    exec(&format!("sepfile {} {}", args[2], args[3]));
-    exec(&format!("natsme {}.1 {} {}", args[2], args[4], args[5]));
+    let src = load(&args[2]);
+    let from: i64 = args[3].parse::<i64>().unwrap() + 173;
+    let len: i64 = (src.len() as i64) - from - 135;
+    let mut out: Vec<u8> = Vec::new();
+    for i in from..len {
+        out.push(src[i as usize]);
+    }
+    save("nats.out", out);
+    encrypter::decrypt_one_file("nats.out", &args[4]);
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
