249a58ded5467ff08f1426a62ab6fda16fe7531f
takuto <takuto.yoshikai@gmail.com>
2020-08-31 20:29:13 +0900

    nats_config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use std::env;
 use std::fs::File;
 use std::fs;
+use std::io::BufReader;
 use std::io::{Write, Read};
+use dirs;
 extern crate rand;
 mod encryptor;
+use serde::{Serialize, Deserialize};
 
 //change this 
 const FIRST_OFFSET_LENGTH: i64 = 173;
 //change this 
 const LAST_OFFSET_LENGTH: i64 = 135;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    first_offset_length: i64,
+    last_offset_length: i64,
+}
+fn config_path() -> String {
+    let mut path = dirs::home_dir().unwrap();
+    path.push(".nats_config.json");
+    return path.to_str().unwrap().to_string();
+}
+
+fn load_config() -> (i64, i64) {
+    let path: &str = &config_path();
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => return (FIRST_OFFSET_LENGTH, LAST_OFFSET_LENGTH),
+    };
+    let reader = BufReader::new(file);
+    let deserialized: Config = match serde_json::from_reader(reader) {
+        Ok(config) => config,
+        Err(_) => return (FIRST_OFFSET_LENGTH, LAST_OFFSET_LENGTH),
+    };
+    return (deserialized.first_offset_length, deserialized.last_offset_length);
+}
 
 fn random_bytes(n: i64) -> Vec<u8>{
     return (0..n).map( |_| {
@@ -30,31 +59,25 @@ fn save(filename: &str, data: &Vec<u8>) {
     file.flush().expect("failed to flush");
 }
 
-fn first_offset() -> Vec<u8> {
-    return random_bytes(FIRST_OFFSET_LENGTH);
-}
 
-fn last_offset() -> Vec<u8> {
-    return random_bytes(LAST_OFFSET_LENGTH);
-}
-
-fn pack(command: &Vec<u8>, data: &Vec<u8>) -> Vec<u8> {
+fn pack(command: &Vec<u8>, data: &Vec<u8>, first_offset_length: i64, last_offset_length: i64) -> Vec<u8> {
     let mut result: Vec<u8> = command.clone();
-    result.extend(&first_offset());
+    result.extend(&random_bytes(first_offset_length));
     result.extend(data);
-    result.extend(&last_offset());
+    result.extend(&random_bytes(last_offset_length));
     return result;
 }
 
 fn nats_in() {
     let args: Vec<String> = env::args().collect();
+    let (first_offset_length, last_offset_length) = load_config();
     if args.len() >= 5 {
         let keypath: &str = &args[4];
         let (key, iv) = encryptor::load_key(keypath);
         let mut data = load(&args[3]);
         data = encryptor::encrypt(&data, &key, &iv).unwrap();
         let command = load(&args[2]);
-        let packed = pack(&command, &data);
+        let packed = pack(&command, &data, first_offset_length, last_offset_length);
         save(&format!("{}.dm", args[2]), &packed);
         return;
     }
@@ -62,15 +85,16 @@ fn nats_in() {
     encryptor::encrypt_one_file(&args[3], filename);
     let command = load(&args[2]);
     let data = load(filename);
-    let packed = pack(&command, &data);
+    let packed = pack(&command, &data, first_offset_length, last_offset_length);
     save(&format!("{}.dm", args[2]), &packed);
 }
 
 fn nats_out() {
     let args: Vec<String> = env::args().collect();
+    let (first_offset_length, last_offset_length) = load_config();
     let src = load(&args[2]);
-    let from: i64 = args[3].parse::<i64>().unwrap() + FIRST_OFFSET_LENGTH;
-    let len: i64 = (src.len() as i64) - from - LAST_OFFSET_LENGTH;
+    let from: i64 = args[3].parse::<i64>().unwrap() + first_offset_length;
+    let len: i64 = (src.len() as i64) - from - last_offset_length;
     let mut out: Vec<u8> = Vec::new();
     for i in 0..len {
         out.push(src[(from + i) as usize]);
